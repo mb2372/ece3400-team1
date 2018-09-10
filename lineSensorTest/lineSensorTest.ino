08028645d1da679bd2d0b1e3c1b1881f1ab74a4f
mb2372 <mb2372@cornell.edu>
2018-09-10 17:27:08 -0400

    update turning

--- lineSensorTest/lineSensorTest.ino
+++ lineSensorTest/lineSensorTest.ino
@@ -1,8 +1,12 @@
-void setup() {
-  Serial.begin(9600);
-}
+#include <Servo.h>
+Servo servoLeft; //left servo 
+Servo servoRight; //right servo
 
+void setup() {
+  servoLeft.attach(9); //left servo
+  servoRight.attach(10); //right servo 
+}
 void loop() {
-  double sensorValue=analogRead(A0);
-  Serial.println(sensorValue);
+  servoLeft.write(87);
+  servoRight.write(87);
 }
--- lineSensors/lineSensors.ino
+++ lineSensors/lineSensors.ino
@@ -4,14 +4,57 @@ Servo servoRight; //right servo
 double lsensorR; //right line sensor
 double lsensorL; //left line sensor
 double lsensorM; //middle line sensor
+double threshold=900.0; 
 
 void setup() {
-  servoLeft.attach(9);
-  servoRight.attach(10);
+  servoLeft.attach(9); //left servo
+  servoRight.attach(10); //right servo 
+  /*
+  Serial.begin(9600);
+  servoRight.write(90);
+  servoLeft.write(90);
+  */
 }
 
 void loop() {
   lsensorR = analogRead(A0);
-  lsensorL = analogRead(A0);
-  lsensorM = analogRead(A0);
+  lsensorL = analogRead(A1);
+  lsensorM = analogRead(A2);
+  /*
+  Serial.print("Left Sensor: ");
+  Serial.println(lsensorL);
+  Serial.print("Right Sensor: ");
+  Serial.println(lsensorR);
+  Serial.print("Middle Sensor: ");
+  Serial.println(lsensorM);
+  Serial.print("-----------------------");
+  delay(3000);
+  */
+  //move forward
+    if(lsensorR>=threshold && lsensorL>=threshold && lsensorM<threshold){
+    servoRight.write(0);
+    servoLeft.write(180);
+    }
+  //right turn 
+    if(lsensorR<threshold && lsensorL>=threshold && lsensorM>=threshold){
+    servoRight.write(95);
+    servoLeft.write(98);
+    }
+  //left turn 
+    if(lsensorR>=threshold && lsensorL<threshold && lsensorM>=threshold){
+    servoRight.write(85);
+    servoLeft.write(88);
+    }
+  //all sensors are on black 
+    if(lsensorR>=threshold && lsensorL>=threshold && lsensorM>=threshold){
+    servoRight.write(0);
+    servoLeft.write(180);
+    }
+  //intersection detection (all sensors are white)
+    if(lsensorR<threshold && lsensorL<threshold && lsensorM<threshold){
+    servoRight.write(95);
+    servoLeft.write(95);
+    delay(50);
+    }
+ 
 }
