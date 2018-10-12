adfda8a4e16d79837c76574a2085437bb39870f0
mb2372 <mb2372@cornell.edu>
2018-10-12 19:11:17 -0400

    updated wall detection

--- lineSensors/lineSensors.ino
+++ lineSensors/lineSensors.ino
@@ -3,65 +3,73 @@
 
 //TODO: INCLUDE THE FFT FOR IR AND MICROPHONE. FIX FREE RUNNING MODE VS ANALOGREAD CONFLICT
 
+
+//declaring servors
+//<90 is clockwise
+//>90 is counterclockwise
 Servo servoLeft; //left servo 
 Servo servoRight; //right servo
+
+//declaring line sensors & black/white threshold
 double lsensorR; //right line sensor
 double lsensorL; //left line sensor
 double lsensorM; //middle line sensor
-double threshold=900.0; //less than is white, greater than is black
-//for the mux with wall sensors as the inputs
+double line_threshold=900.0; //less than is white, greater than is black
+
+//declaring wall sensors & wall/no wall threshold
+double l_wall_sensor; //left wall sensor
+double r_wall_sensor; //right wall sensor
+double f_wall_sensor; //front wall sensor
+double wall_threshold=250.0;//less is no wall, > is wall
+
+//initializing select pins for the mux with wall sensors
 int S0 = 7;//pin 7 is S0
 int S1 = 6;//pin 6 is S1
-
-//servors
-//<90 is clockwise
-//>90 is counterclockwise
 
 
 void setup() {
   servoLeft.attach(9); //left servo
   servoRight.attach(10); //right servo 
-  pinMode(LED_BUILTIN,OUTPUT);//testing when we are at intersection or correcting. This can be removed later
-  
-  //setup for the mux and the wall sensors
-  //E AND S2 ARE ALWAYS LOW FOR MUX TO WORK
-//      S1     S0      CHANNEL
-//      L      L         A0            
-//      L      H         A1            
-//      H      L         A2       
-  pinMode(S0,OUTPUT);//S0
-  pinMode(S1,OUTPUT);//S1
+  pinMode(LED_BUILTIN,OUTPUT);//testing when we are at intersection or correcting. This can be removed later  
+  pinMode(S0,OUTPUT);//input select signal 0 for mux
+  pinMode(S1,OUTPUT);//input select signal 1 for mux
   pinMode(A5, INPUT);//output of the mux, input into the Arduino. Need a resistor for this
-  Serial.begin(9600);//used for looking at
+  Serial.begin(9600);
 }
 
 //TODO: METHOD FOR CHECKING MICROPHONE TO KNOW WHEN WE START
 
 
-//method for wall detecting
+//METHODS FOR WALL DETECTION
+//////////////////////////////////////////
+//setup for the mux and the wall sensors
+//E AND S2 ARE ALWAYS LOW FOR MUX TO WORK
+//      S1     S0      WALL SENSOR
+//      L      L          LEFT            
+//      L      H          RIGHT            
+//      H      L          FRONT  
+//////////////////////////////////////////
+
 void left_wall_detect(){
-  //selecting A0, Low Low
+  //setting mux select signals
   digitalWrite(S1, LOW);
   digitalWrite(S0, LOW);
-  int outA0 = analogRead(A5);
-  Serial.println("channel input A0: "+outA0);
+  l_wall_sensor = analogRead(A5);
 }
 void right_wall_detect(){
-  //selecting A1, Low High
+  //setting mux select signals
   digitalWrite(S1, LOW);
   digitalWrite(S0, HIGH);
-  int outA1 = analogRead(A5);
-  Serial.println("channel input A1: "+outA1);
+  r_wall_sensor = analogRead(A5);
 }
 void front_wall_detect(){
-  //selecting A2, High Low
+  //setting mux select signals
   digitalWrite(S1,HIGH);
   digitalWrite(S0,LOW);
-  int outA2 = analogRead(A5);
-  Serial.println("channel input A2: "+outA2);
+  f_wall_sensor = analogRead(A5);
 }
 
-//TURNING METHODS
+//METHODS FOR TURNING/MOVING FORWARD/STOPPING
 //a turn 90 to the right
 void right_turn(){
   servoLeft.write(180);
@@ -76,9 +84,9 @@ void left_turn(){
 }
 //correction, not a turn
 void slight_right(){
-    servoRight.write(95);
-    servoLeft.write(98);
-    delay(100);
+  servoRight.write(95);
+  servoLeft.write(98);
+  delay(100);
 }
 //correction, not a turn
 void hard_right(){
@@ -98,14 +106,16 @@ void hard_left(){
   servoLeft.write(85);
   delay(100);
 }
-
+//move forward
 void forward(){
   servoRight.write(0);
   servoLeft.write(180);  
 }
-
-
-
+//stop
+void pause(){
+  servoRight.write(90);
+  servoLeft.write(90);  
+}
 void loop() {
   lsensorR = analogRead(A0);
   lsensorL = analogRead(A1);
@@ -127,47 +137,56 @@ void loop() {
   //TODO: CONSTANTLY CHECK IR FOR OTHER ROBOTS
 
   //intersection detection (back sensors are white)
-    if(lsensorR<threshold && lsensorL<threshold && lsensorM<threshold){
-      //servoLeft.write(90);
-      //servoRight.write(90);
-      //delay(10);
-      
+    if(lsensorR<line_threshold && lsensorL<line_threshold && lsensorM<line_threshold){
       //TODO: STOP AND DO WALL DETECTION, THEN DECIDE HOW TO MOVE
       //maybe set the two servos both to 90 to do a brief stop and analyze
-      
       forward();
       delay(240);//bump the robot forward a little bit
-      //currently defaulted to left turn. Will need to change
-      left_turn();
-    }
+      pause();
+      front_wall_detect();
+      right_wall_detect();
+      left_wall_detect();
+      delay(500);
+      //no wall in the front and no wall on right or left
+      if(l_wall_sensor>=wall_threshold && r_wall_sensor>=wall_threshold && f_wall_sensor<wall_threshold){
+        forward();
+      }
+      //no wall on the right and walls on left and front
+      if(l_wall_sensor>=wall_threshold && r_wall_sensor<wall_threshold && f_wall_sensor>=wall_threshold){
+        right_turn();
+      }
+      //no wall on the left and walls on right and front
+      if(l_wall_sensor<wall_threshold && r_wall_sensor>=wall_threshold && f_wall_sensor>=wall_threshold){
+        left_turn();
+      }
+     }
     //not at an intersection: line following and correcting
     else{
       //just here for testing intersection vs not intersection. Can delete later
       digitalWrite(LED_BUILTIN, HIGH);
       
       //hard right correction: right is white, left and middle are black
-      if(lsensorR<threshold && lsensorL>=threshold && lsensorM>=threshold){
+      if(lsensorR<line_threshold && lsensorL>=line_threshold && lsensorM>=line_threshold){
         hard_right();
       }
     //slight right correction: right is white, left is black, middle is white
-      else if(lsensorR<threshold && lsensorL>=threshold && lsensorM<threshold){
+      else if(lsensorR<line_threshold && lsensorL>=line_threshold && lsensorM<line_threshold){
           slight_right();
       }
     //left correction: left is white and middle and right are black
-      else if(lsensorR>=threshold && lsensorL<threshold && lsensorM>=threshold){
+      else if(lsensorR>=line_threshold && lsensorL<line_threshold && lsensorM>=line_threshold){
         hard_left();
       }
      //slight left correction: left and middle are white and right is black
-      else if(lsensorR>=threshold && lsensorL<threshold && lsensorM<threshold){
+      else if(lsensorR>=line_threshold && lsensorL<line_threshold && lsensorM<line_threshold){
         slight_left();
       }
         //move forward:right is black, left is black
-      else if(lsensorR>=threshold && lsensorL>=threshold && lsensorM<threshold){
+      else if(lsensorR>=line_threshold && lsensorL>=line_threshold && lsensorM<line_threshold){
         forward();
       }
       //just here for testing intersection vs not intersection. Can delete later
       digitalWrite(LED_BUILTIN, LOW);
-      
    }
 }
 
