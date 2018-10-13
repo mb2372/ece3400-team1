a90acc554f92b5b4ee305c4222c3be3421063e07
Beau Si <bs655@cornell.edu>
2018-10-13 12:41:35 -0400

    added IR

--- lineSensors/lineSensors.ino
+++ lineSensors/lineSensors.ino
@@ -1,3 +1,5 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
 #include <Servo.h>
 #include <FFT.h> // include the FFT library
 
@@ -27,6 +29,10 @@ int S0 = 7;//pin 7 is S0
 int S1 = 6;//pin 6 is S1
 
 
+//IR stuff
+double ir_sensor;
+double ir_threshold = 110.0;
+
 void setup() {
   servoLeft.attach(9); //left servo
   servoRight.attach(10); //right servo 
@@ -34,11 +40,12 @@ void setup() {
   pinMode(S0,OUTPUT);//input select signal 0 for mux
   pinMode(S1,OUTPUT);//input select signal 1 for mux
   pinMode(A5, INPUT);//output of the mux, input into the Arduino. Need a resistor for this
-  Serial.begin(9600);//for testing the wall output, can delete later
+  //Serial.begin(9600);//for testing the wall output, can delete later
 }
 
 //TODO: METHOD FOR CHECKING MICROPHONE TO KNOW WHEN WE START
 
+//---------------------------------------------------------------------------------------------------------------
 
 //METHODS FOR WALL DETECTION
 //////////////////////////////////////////
@@ -68,6 +75,11 @@ void front_wall_detect(){
   digitalWrite(S0,LOW);
   f_wall_sensor = analogRead(A5);
 }
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------
 
 //METHODS FOR TURNING/MOVING FORWARD/STOPPING
 //a turn 90 to the right
@@ -116,10 +128,63 @@ void pause(){
   servoRight.write(90);
   servoLeft.write(90);  
 }
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+//METHOD FOR IR
+void ir_read(){
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+ 
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();//interrupts
+    ir_sensor=fft_log_out[43];
+
+    
+    //Serial.println("start");
+    /*for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+      Serial.println("Bin Number: " + i);
+      Serial.println(fft_log_out[i]); // send out the data
+      
+    }*/
+    
+}
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+
+
 void loop() {
-  lsensorR = analogRead(A0);
+  lsensorR = analogRead(A3);
   lsensorL = analogRead(A1);
   lsensorM = analogRead(A2);
+
+  ir_read();
+  //if IR detected, stop moving (for now)
+  if(ir_sensor>=ir_threshold){
+    pause();
+    delay(1000);  
+  }
 
   //cases for line following and turning
   /*
